0644043fc4acbbc8bc4e4404f87f9cac8d7a3500
appxpy <appxpy@appxpy.com>
2022-12-01 13:41:54 +0300

    init

---
+++ sp2txtbot/app.py
@@ -0,0 +1,56 @@
+import settings
+import logging
+import datetime
+
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils import executor
+
+
+API_TOKEN = settings.BOT_TOKEN
+
+
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
+
+
+async def on_startup(dp):
+    if settings.DEBUG:
+        logger.warning('Бот запущен в режиме отладки')
+        return
+
+    if settings.BOT_SECURE_KEY:
+        await bot.set_webhook(settings.BOT_WEBHOOK_URL, secret_token=settings.BOT_SECURE_KEY, drop_pending_updates=True)
+        return
+
+    await bot.set_webhook(settings.BOT_WEBHOOK_URL, drop_pending_updates=True)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logger.warning('Завершение работы бота')
+
+    # Удаляем вебхук
+    if not settings.DEBUG:
+        await bot.delete_webhook()
+
+    logger.warning('Пока :)')
+
+if __name__ == '__main__':
+    if settings.DEBUG:
+        executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    else:
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=f'/{settings.BOT_TOKEN_HASH}',
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=settings.LISTEN,
+            port=settings.VIRTUAL_PORT,
+        )
---
+++ sp2txtbot/settings.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import logging
+
+from hashlib import sha512
+
+
+# Общие настройки
+
+DEBUG = bool(os.environ.get('DEBUG', False))
+LISTEN = '0.0.0.0'
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# Задаем путь к файлу с логами, если он не существует, то создаем его
+
+LOG_FILE = os.path.join(BASE_PATH, 'sp2txtbot.log')
+if not os.path.exists(LOG_FILE):
+    open(LOG_FILE, 'w').close()
+
+# Задаем конфигурацию логгера
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    format='[%(asctime)s] - [%(levelname)s] - %(name)s - %(message)s',
+    level=logging.DEBUG if DEBUG else logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+# Настройки прокси
+
+VIRTUAL_HOST = os.environ.get('VIRTUAL_HOST', '')             # Виртуальный хост
+VIRTUAL_PORT = os.environ.get('VIRTUAL_PORT', 8080)           # Виртуальный порт
+
+
+# Настройки бота
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')  # Токен бота
+
+if not BOT_TOKEN:
+    logger.critical('Ошибка конфигурации: не указан токен бота')
+    sys.exit(1)
+
+BOT_TOKEN_HASH = sha512(BOT_TOKEN.encode()).hexdigest()                      # Хэш токена бота
+BOT_SECURE_KEY = os.environ.get('BOT_SECURE_KEY')                            # Секретный ключ бота (для проверки вебхуков)
+BOT_WEBHOOK_URL = f'https://{VIRTUAL_HOST}/{BOT_TOKEN_HASH}'                 # URL вебхука
+
+# Настройки базы данных
+
+SQL_ALLOWED_ENGINES = ['sqlite', 'postgresql']
+SQLITE_DB_PATH = os.path.join(BASE_PATH, 'db.sqlite3')
+
+if not os.path.exists(SQLITE_DB_PATH):
+    open(SQLITE_DB_PATH, 'w').close()
+
+SQL_ENGINE = os.environ.get('SQL_ENGINE', 'sqlite')  # Движок базы данных
+
+if SQL_ENGINE not in SQL_ALLOWED_ENGINES:
+    logger.critical('Ошибка конфигурации: недопустимый движок базы данных, допустимые движки: %s', ', '.join(SQL_ALLOWED_ENGINES))
+    sys.exit(1)
+
+SQL_DATABASE = os.environ.get('SQL_DATABASE')  # Имя базы данных
+SQL_USER = os.environ.get('SQL_USER')          # Имя пользователя
+SQL_PASSWORD = os.environ.get('SQL_PASSWORD')  # Пароль
+SQL_HOST = os.environ.get('SQL_HOST')          # Хост
+SQL_PORT = os.environ.get('SQL_PORT')          # Порт
